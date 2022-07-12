4c7ed0908160b220f41d2f4225ae02e03be893a6
sheygs <segun.ekoh@gmail.com>
2022-07-12 18:53:11 +0100

    Add dotenv

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
+const { log } = console;
+
 function sleep(ms) {
         return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
+        const favoriteFood = process.env.FAVORITE_FOOD;
+        log(`My favorite food is ${favoriteFood}`);
+
         while (true) {
                 console.log('Containers rule!');
                 await sleep(5000);
